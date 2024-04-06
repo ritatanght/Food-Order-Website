a4d45067a7f615506c45858cb44e3db2db4cd1f1
Rita Tang <doggymint@gmail.com>
2024-04-05 21:08:11 -0400

    Add page for admin and link up the stylesheet

---
+++ admin/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Food Order Website - Home Page</title>
+  <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+
+  <h1 class="heading">Admin</h1>
+  <!-- Menu Section Starts -->
+  <!-- Menu Section Ends -->
+
+  <!-- Main Content Section Starts -->
+  <!-- Main Content Section Ends -->
+
+
+  <!-- Footer Section Starts -->
+  <!-- Footer Section Ends -->
+</body>
+
+</html>

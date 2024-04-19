88dd9c1ce9d22fcc208cffd18990050f8a02c1b3
Rita Tang <doggymint@gmail.com>
2024-04-19 19:19:18 -0400

    Add admin login form page and styles

---
+++ admin/login.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login - Food Order System</title>
+  <link rel="stylesheet" href="../css/admin.css">
+</head>
+
+<body>
+
+  <div class="text-center login">
+    <h1>Login</h1>
+    <form action="" method="POST">
+      <label>
+        Username:
+        <input type="text" name="username" placeholder="Enter Username" />
+      </label>
+      <label>
+
+        Password:
+        <input type="password" name="password" placeholder="Enter Password" />
+      </label>
+
+      <input type="submit" name="submit" value="Login" class="btn-primary" />
+    </form>
+    <p class=" text-center">Creatd By - <a href="https://github.com/ritatanght">Rita Tang</a></p>
+  </div>
+
+</body>
+
+</html>

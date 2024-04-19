ab3c9971d934b4475b0702a580ea9dfce7ac2a15
Rita Tang <doggymint@gmail.com>
2024-04-18 21:51:28 -0400

    Add the form for updating password

--- admin/update-password.php
+++ admin/update-password.php
@@ -1,3 +1,43 @@
-<?php
+<?php include './partials/menu.php' ?>
+<div class="main-content">
+  <div class="wrapper">
+    <h1>Change Password</h1>
+    <br />
 
-echo "Update password for admin {$_GET['id']}";
+    <?php
+    if (isset($_GET['id'])) {
+      $id = $_GET['id'];
+    }
+    ?>
+
+    <form action="" method="POST">
+      <table class="tbl-30">
+        <tr>
+          <td>Current Password:</td>
+          <td><input type="password" name="current_password" placeholder="Current Password" /></td>
+        </tr>
+
+        <tr>
+          <td>New Password:</td>
+          <td><input type="password" name="new_password" placeholder="New Password" /></td>
+        </tr>
+
+        <tr>
+          <td>Confirm Password:</td>
+          <td><input type="password" name="confirm_password" placeholder="Confirm Password" /></td>
+        </tr>
+
+        <tr>
+          <td colspan="2">
+            <input type="hidden" name="id" value="<?= $id ?>" />
+            <input type="submit" name="submit" value="Change Password" class="btn-secondary" />
+          </td>
+
+        </tr>
+      </table>
+    </form>
+
+  </div>
+</div>
+
+<?php include './partials/footer.php' ?>

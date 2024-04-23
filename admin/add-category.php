3a8b7ba0e921b64e3a6017e473e4fc2f81cc82df
Rita Tang <doggymint@gmail.com>
2024-04-23 18:32:15 -0400

    Add input and functionality to upload image on submission of form

--- admin/add-category.php
+++ admin/add-category.php
@@ -11,11 +11,18 @@
     }
     ?>
 
-    <form action="" method="post">
+    <form action="" method="post" enctype="multipart/form-data">
       <table class="tbl-30">
         <tr>
           <td>Title: </td>
           <td><input type="text" name="title" placeholder="Category Title" /></td>
+        </tr>
+
+        <tr>
+          <td>Select Image: </td>
+          <td>
+            <input type="file" name="image" />
+          </td>
         </tr>
 
         <tr>
@@ -51,10 +58,31 @@ if (isset($_POST['submit'])) {
 
   extract($_POST);
 
+  // check whether the image is selected
+  var_dump($_FILES['image']);
+
+  if (isset($_FILES['image']['name'])) {
+    // we need image name, source path and destination path for upload
+    $image_name = $_FILES['image']['name'];
+    $source_path = $_FILES['image']['tmp_name'];
+    $destination_path = "../images/category/{$image_name}";
+
+    $upload = move_uploaded_file($source_path, $destination_path);
+
+    // if the image is failed to be uploaded, stop the process and redirect with error message
+    if (!$upload) {
+      $_SESSION['flash'] = "<div class='flash-message error'>Failed to Upload Image</div>";
+      header("location: /admin/add-category.php");
+      die();
+    }
+  } else {
+    $image_name = "";
+  }
+
   $res = $conn->execute_query(
-    "INSERT INTO tbl_category (title, featured, active) 
-    VALUES (?,?,?)",
-    [$title, $featured, $active]
+    "INSERT INTO tbl_category (title, image_name, featured, active) 
+      VALUES (?,?,?,?)",
+    [$title, $image_name, $featured, $active]
   );
 
   if ($res) {
